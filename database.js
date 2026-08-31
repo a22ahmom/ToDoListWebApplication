84974796312522250be526c34058070a1087e16e
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-31 10:55:01 +0200

    created the table tasks in the database tasks.db

--- database.js
+++ database.js
@@ -2,4 +2,12 @@ const Database = require("better-sqlite3");
 
 const db = new Database("tasks.db");
 
+db.exec(`
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        text TEXT NOT NULL,
+        completed INTEGER DEFAULT 0
+    )
+`);
+
 console.log("Database connected!");
