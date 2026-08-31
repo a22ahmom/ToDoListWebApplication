2fc7a85bb3a7434807d387bb447d6fbcf4879127
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-31 11:07:40 +0200

    added express as the library to create a web server between the frontend and database

--- database.js
+++ database.js
@@ -10,4 +10,19 @@ db.exec(`
     )
 `);
 
+// const insertTask = db.prepare(`
+//     INSERT INTO tasks (text)
+//     VALUES (?)
+// `);
+
+// insertTask.run("Learn SQLite");
+
+const readTask = db.prepare(`
+
+    SELECT * FROM tasks
+
+`).all();
+
+console.log(readTask);
+
 console.log("Database connected!");
