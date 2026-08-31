9053eaab8f248be7a019ab5179998a6077e182f5
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-31 11:23:31 +0200

    Created the first API endpoint which is a GET request

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.use(express.json());
 
 const PORT = 3000;
 
+app.get("/tasks", (req, res) => {
+
+    const tasks = db.prepare(`
+        SELECT * FROM tasks
+    `).all();
+
+    res.json(tasks);
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
