2f8603e87e04b5961f8594f95b67b8c9cfdbfb27
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-09-05 10:52:33 +0200

    Implemented functionality to add a task in the UI and save it in the database file

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const db = require("./database");
+// const path = require("path");
 
 const app = express();
 
+// app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 const PORT = 3000;
 
@@ -14,6 +17,24 @@ app.get("/tasks", (req, res) => {
     `).all();
 
     res.json(tasks);
+});
+
+app.post("/tasks", (req, res) => {
+    
+    const text = req.body.text;
+
+    const insertTask = db.prepare(`
+        INSERT INTO tasks (text)
+        VALUES (?)
+    `);
+
+    const result = insertTask.run(text);
+
+    res.json({
+        id: result.lastInsertRowid,
+        text: text,
+        completed: 0
+    });
 });
 
 app.listen(PORT, () => {
